d53e1214184e0e3d6516aa19d351e06d8f3781f0
mixxuss <mixxuss@gmail.com>
2017-01-17 13:38:04 +0300

    added scope in vk.AuthSession

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -17,17 +17,20 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
+    #api.
     return api.friends.getOnline
 
+# api.login(login, password)
 
 def output_friends_to_console(friends_online):
-    print(friends_online)
+    return friends_online
 
 if __name__ == '__main__':
     # login = get_user_login()
     # password = get_user_password()
     friends_online = get_online_friends('mixxuss@gmail.com', 'Arshavin-93')
-    type(friends_online)
-    # output_friends_to_console(friends_online)
+    # type(friends_online)
+    output_friends_to_console(friends_online)
